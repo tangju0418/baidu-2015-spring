38222984ea4215475ce0bc525fc5c385122dc0cb
tangju0418 <635465547@qq.com>
2016-04-17 00:40:01 +0800

    2048 js代码的完善

--- 2048/js/2048.js
+++ 2048/js/2048.js
@@ -1,7 +1,7 @@
-
+//(function(){
 
 //// TODO: 全局变量？就算不能完全取消全局变量，能不能减少数量?
-var grid = [];          //// 命名方式不合适
+var gameExcel = [];
 var emptyDivs = [];
 var scores = parseInt(document.getElementById("score").innerHTML);
 const row = 4;
@@ -17,6 +17,8 @@ function addLoadEvent(func){
         window.onload = function(){
             oldonload();
             func();
+
+            //alert(1);
         }
     }
 }
@@ -24,17 +26,22 @@ function addLoadEvent(func){
 addLoadEvent(initGridItems);
 addLoadEvent(initGame);
 
+//alert(3);
+//window.onload();
+//alert(3);
 
 function initGame(){
     emptyDivs = [];
+    scores = 0;
+    document.getElementById("score").innerHTML = scores;
     var endPage = document.getElementById("end");
     endPage.style.display="none";
     //// TODO: 出现了很多次的4 这个魔幻数字，修改
     for(var i=0;i < row;i++) {
         for (var j = 0; j < col; j++) {
-            setDivValue(grid[i][j],invalidValue);
-            grid[i][j].innerHTML = invalidValue;
-            grid[i][j].setAttribute("class","myGrid");
+            setDivValue(gameExcel[i][j],invalidValue);
+            gameExcel[i][j].innerHTML = invalidValue;
+            gameExcel[i][j].setAttribute("class","myGrid");
         }
     }
 
@@ -58,7 +65,7 @@ function initGridItems() {
             game.appendChild(dv);
             arr.push(dv);
         }
-        grid.push(arr);
+        gameExcel.push(arr);
     }
 }
 
@@ -72,6 +79,15 @@ function makeClassName(i){
 
 function setDivValue(divElem, value){
     divElem.setAttribute("GameValue", value);
+    divElem.innerHTML = value;
+    if(value === invalidValue){
+        divElem.setAttribute("class", "myGrid");
+    }
+    else{
+        divElem.setAttribute("class", makeClassName(value));
+    }
+
+
 }
 
 function getDivValue(divElem){
@@ -83,8 +99,8 @@ function refreshEmptyDivs(empty_divs){
 
     for(var i=0;i<row;i++){
         for(var j=0;j<col;j++){
-            if(invalidValue === getDivValue(grid[i][j])){
-                empty_divs.push(grid[i][j]);
+            if(invalidValue === getDivValue(gameExcel[i][j])){
+                empty_divs.push(gameExcel[i][j]);
             }
         }
     }
@@ -95,42 +111,30 @@ function refreshEmptyDivs(empty_divs){
 function fillOneDivItem(empty_divs){
     var nNumber =Math.round(Math.random()*(empty_divs.length-1));
     var num=(Math.random()<0.8 ? 2 : 4);
-
-    empty_divs[nNumber].innerHTML=num;
-    setDivValue(empty_divs[nNumber], empty_divs[nNumber].innerHTML);
-    empty_divs[nNumber].setAttribute("class",makeClassName(num));
+    setDivValue(empty_divs[nNumber], num);
 }
 
 //// TODO: leftAddNum是什么意思？
 //// TODO: 这个函数必须这么长？缩短
-function leftAddNum(divs) {
+function leftMoveSum(divs) {
     var add = false;
     for (var i = 0; i < row; i++) {
         for (var j = 0; j < col-1; j++) {
-            if (grid[i][j].innerHTML != invalidValue && grid[i][j].innerHTML == grid[i][j+1].innerHTML) {
-                setDivValue(grid[i][j], 2 * grid[i][j].innerHTML);
-                grid[i][j].innerHTML = getDivValue(grid[i][j]);
-                grid[i][j].setAttribute("class", makeClassName(getDivValue(grid[i][j])));
-                add = true;
+            if (gameExcel[i][j].innerHTML != invalidValue && gameExcel[i][j].innerHTML == gameExcel[i][j+1].innerHTML) {
+                setDivValue(gameExcel[i][j], 2 * gameExcel[i][j].innerHTML);
+                setDivValue(gameExcel[i][j+1], invalidValue);
 
-                scores += parseInt(getDivValue(grid[i][j]));
+                add = true;
+                scores += parseInt(getDivValue(gameExcel[i][j]));
                 document.getElementById("score").innerHTML = scores;
 
-                setDivValue(grid[i][j+1], invalidValue);
-                grid[i][j+1].innerHTML = invalidValue;
-                grid[i][j+1].setAttribute("class", "myGrid");
-                if((j+1)==1 && grid[i][j+2].innerHTML != invalidValue && grid[i][j+2].innerHTML == grid[i][j+3].innerHTML){
-                    setDivValue(grid[i][j+2], 2 * grid[i][j+2].innerHTML);
-                    grid[i][j+2].innerHTML = getDivValue(grid[i][j+2]);
-                    grid[i][j+2].setAttribute("class", makeClassName(getDivValue(grid[i][j+2])));
+                if((j+1)==1 && gameExcel[i][j+2].innerHTML != invalidValue && gameExcel[i][j+2].innerHTML == gameExcel[i][j+3].innerHTML){
+                    setDivValue(gameExcel[i][j+2], 2 * gameExcel[i][j+2].innerHTML);
+                    setDivValue(gameExcel[i][j+3], invalidValue);
+
                     add = true;
-
-                    scores += parseInt(getDivValue(grid[i][j+2]));
+                    scores += parseInt(getDivValue(gameExcel[i][j+2]));
                     document.getElementById("score").innerHTML = scores;
-
-                    setDivValue(grid[i][j+3], invalidValue);
-                    grid[i][j+3].innerHTML = invalidValue;
-                    grid[i][j+3].setAttribute("class", "myGrid");
                 }
             }
         }
@@ -141,14 +145,14 @@ function leftAddNum(divs) {
 
 //// TODO: 修改为递归形式
 //// TODO: left_move 和 up_move很像，能不能合并?
-function left_move(divs){
+function leftMove(divs){
     var move = false;
     for(var i=0;i<row;i++){
         var m = -1;
         for (var j = 0; j < divs[i].length; ++j){
             //// TODO: 魔幻比较方式，建议修改
             if (invalidValue === getDivValue(divs[i][j])){
-                if (-1 == m){
+                if (-1 === m){
                     m = j;
                 }
             }
@@ -156,12 +160,7 @@ function left_move(divs){
                 if (-1 != m){
                     //// TODO: 这个地方建议修改，逻辑太乱
                     setDivValue(divs[i][m], getDivValue(divs[i][j]));
-                    divs[i][m].innerHTML = getDivValue(divs[i][m]);
-                    divs[i][m].setAttribute("class",makeClassName(getDivValue(divs[i][m])));
-
                     setDivValue(divs[i][j], invalidValue);
-                    divs[i][j].innerHTML = invalidValue;
-                    divs[i][j].setAttribute("class","myGrid");
                     m++;
                     move = true;
                 }
@@ -171,44 +170,36 @@ function left_move(divs){
     return move;
 }
 
-//// TODO: 函数命名不合适
-function leftRightReverse(divs){
+//// TODO: leftRightReverse函数命名不合适
+function rowReverse(divs){
     for (var i=0;i<row;i++){
         divs[i].reverse();
     }
     return divs;
 }
 
+
 //// TODO: upAddNum是什么意思？
 //// TODO: 这个函数必须这么长？缩短
-function upAddNum(divs) {
+function upMoveSum(divs) {
     var add = false;
     for (var j = 0; j < col; j++) {
         for (var i = 0; i < row-1; i++) {
-            if (grid[i][j].innerHTML != invalidValue && grid[i][j].innerHTML == grid[i + 1][j].innerHTML) {
-                setDivValue(grid[i][j], 2 * grid[i][j].innerHTML);
-                grid[i][j].innerHTML = getDivValue(grid[i][j]);
-                grid[i][j].setAttribute("class", makeClassName(getDivValue(grid[i][j])));
-                add = true;
+            if (gameExcel[i][j].innerHTML != invalidValue && gameExcel[i][j].innerHTML == gameExcel[i + 1][j].innerHTML) {
+                setDivValue(gameExcel[i][j], 2 * gameExcel[i][j].innerHTML);
+                setDivValue(gameExcel[i + 1][j], invalidValue);
 
-                scores += parseInt(getDivValue(grid[i][j]));
+                add = true;
+                scores += parseInt(getDivValue(gameExcel[i][j]));
                 document.getElementById("score").innerHTML = scores;
 
-                setDivValue(grid[i + 1][j], invalidValue);
-                grid[i + 1][j].innerHTML = invalidValue;
-                grid[i + 1][j].setAttribute("class", "myGrid");
-                if ((i + 1) == 1 && grid[i + 2][j].innerHTML != invalidValue && grid[i + 2][j].innerHTML == grid[i + 3][j].innerHTML) {
-                    setDivValue(grid[i + 2][j], 2 * grid[i + 2][j].innerHTML);
-                    grid[i+2][j].innerHTML = getDivValue(grid[i+2][j]);
-                    grid[i+2][j].setAttribute("class", makeClassName(getDivValue(grid[i+2][j])));
+                if ((i + 1) == 1 && gameExcel[i + 2][j].innerHTML != invalidValue && gameExcel[i + 2][j].innerHTML == gameExcel[i + 3][j].innerHTML) {
+                    setDivValue(gameExcel[i + 2][j], 2 * gameExcel[i + 2][j].innerHTML);
+                    setDivValue(gameExcel[i+3][j], invalidValue);
+
                     add = true;
-
-                    scores += parseInt(getDivValue(grid[i+2][j]));
+                    scores += parseInt(getDivValue(gameExcel[i+2][j]));
                     document.getElementById("score").innerHTML = scores;
-
-                    setDivValue(grid[i+3][j], invalidValue);
-                    grid[i+3][j].innerHTML = invalidValue;
-                    grid[i+3][j].setAttribute("class", "myGrid");
                 }
             }
         }
@@ -217,7 +208,7 @@ function upAddNum(divs) {
 }
 
 
-function up_move(divs){
+function upMove(divs){
     var move = false;
     for(var j=0;j<col;j++){
         var m = -1;
@@ -230,11 +221,7 @@ function up_move(divs){
             else{
                 if (-1 != m){
                     setDivValue(divs[m][j], getDivValue(divs[i][j]));
-                    divs[m][j].innerHTML = getDivValue(divs[m][j]);
-                    divs[m][j].setAttribute("class",makeClassName(getDivValue(divs[m][j])));
                     setDivValue(divs[i][j],invalidValue);
-                    divs[i][j].innerHTML = invalidValue;
-                    divs[i][j].setAttribute("class","myGrid");
                     m++;
                     move = true;
                 }
@@ -244,7 +231,7 @@ function up_move(divs){
     return move;
 }
 
-function upDownReverse(divs){
+function cloReverse(divs){
     for (var j=0;j<col;j++){
         for(var i=0;i<row/2;i++){
             var arr = divs[i][j];
@@ -262,9 +249,9 @@ document.onkeydown=function(e){
     //// TODO：相比switch case，有没有更好的方式?
     switch(e.keyCode){
         case 37: //左键
-            var moveLeft = left_move(grid);
-            var addLeft = leftAddNum(grid);
-            left_move(grid);
+            var moveLeft = leftMove(gameExcel);
+            var addLeft = leftMoveSum(gameExcel);
+            leftMove(gameExcel);
 
             if(moveLeft || addLeft){
                 refreshEmptyDivs(emptyDivs);
@@ -280,9 +267,9 @@ document.onkeydown=function(e){
 
             break;
         case 38: //向上键
-            var moveUp = up_move(grid);
-            var addUp = upAddNum(grid);
-            up_move(grid);
+            var moveUp = upMove(gameExcel);
+            var addUp = upMoveSum(gameExcel);
+            upMove(gameExcel);
 
             if(moveUp || addUp) {
                 refreshEmptyDivs(emptyDivs);
@@ -297,11 +284,11 @@ document.onkeydown=function(e){
             break;
 
         case 39: //右键
-            leftRightReverse(grid);
-            var moveRight =left_move(grid);
-            var addRight =leftAddNum(grid);
-            left_move(grid);
-            leftRightReverse(grid);
+            rowReverse(gameExcel);
+            var moveRight =leftMove(gameExcel);
+            var addRight =leftMoveSum(gameExcel);
+            leftMove(gameExcel);
+            rowReverse(gameExcel);
 
             if(moveRight || addRight) {
                 refreshEmptyDivs(emptyDivs);
@@ -316,11 +303,11 @@ document.onkeydown=function(e){
             break;
 
         case 40: //向下键
-            upDownReverse(grid);
-            var moveDown = up_move(grid);
-            var addDown = upAddNum(grid);
-            up_move(grid);
-            upDownReverse(grid);
+            cloReverse(gameExcel);
+            var moveDown = upMove(gameExcel);
+            var addDown = upMoveSum(gameExcel);
+            upMove(gameExcel);
+            cloReverse(gameExcel);
 
             if(moveDown || addDown) {
                 refreshEmptyDivs(emptyDivs);
@@ -345,9 +332,111 @@ function gameOver() {
     var gameAgain = document.getElementById("again");
 
     gameAgain.onclick = function(){
-        //// TODO: 这前面的代码为什么不在initGame中？
+        //// FIX: 这前面的代码为什么不在initGame中？
 
         initGame();
     };
 
 }
+
+//})();
+
+
+(function() {
+    var endPage = document.getElementById("end");
+    endPage.style.display="inline";
+    var gameAgain = document.getElementById("again");
+
+    gameAgain.onclick = function(){
+        //// FIX: 这前面的代码为什么不在initGame中？
+
+        initGame();
+    };
+
+})();
+//function Person(n){
+//    var age = 0;
+//    var address = "a";
+//    var name = n;
+//
+//    return {
+//        getName : function (){
+//            return name;
+//        },
+//        changeInfo: function (a, addr){
+//            age = a;
+//            address = addr;
+//        },
+//        getAge: function (){
+//            return age;
+//        },
+//        getAddress: function (){
+//            return address;
+//        },
+//        getAge1: function (){
+//            return age;
+//        }
+//    };
+//}
+//
+//var p = Person("lsy");
+//alert(p.getName());
+//
+//p.changeInfo(1, "b");
+//alert(p.getAddress());
+//alert(p.getAge());
+//
+
+
+//var f = function (){};
+//function f(){};
+
+
+//var f_test = function (){
+//    alert(2);
+//};  // 匿名函数对象
+//f_test();
+
+
+
+//function f111(){
+//    var iii = 0;
+//    alert(iii);
+//
+//    return function (){
+//        alert(iii);
+//    };
+//}
+//
+//var bbb = f111();
+//bbb();
+
+
+
+//function sum_array(arr, sum_func){
+//    var ret_arr = [];
+//    for (var i = 0; i < arr.length; i = i+2){
+//        ret_arr.push(sum_func(arr[i], arr[i+1]));
+//    }
+//
+//    return ret_arr;
+//}
+//
+//var arr_t = [0,1,2,3,4,5,6,7];
+
+
+//alert(sum_array(arr_t, function (i, j){    return i + j;}));
+
+//alert(sum_array(arr_t, function (i, j){
+//    return i*j;
+//}));
+
+
+//alert(sum_array(arr_t, function (i){
+//    return function (i1,j1){
+//        i+=1;
+//        return i1 + j1 + i;
+//    }
+//}(9)));
+
+
