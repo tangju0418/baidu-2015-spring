1aa690835285b06356df5f9fdd30eca5effd8a80
tangju0418 <635465547@qq.com>
2016-04-17 22:27:58 +0800

    2048 普通版

--- 2048/js/2048.js
+++ 2048/js/2048.js
@@ -1,6 +1,5 @@
-//(function(){
+(function(){
 
-//// TODO: 全局变量？就算不能完全取消全局变量，能不能减少数量?
 var gameExcel = [];
 var emptyDivs = [];
 var scores = parseInt(document.getElementById("score").innerHTML);
@@ -17,8 +16,6 @@ function addLoadEvent(func){
         window.onload = function(){
             oldonload();
             func();
-
-            //alert(1);
         }
     }
 }
@@ -26,24 +23,20 @@ function addLoadEvent(func){
 addLoadEvent(initGridItems);
 addLoadEvent(initGame);
 
-//alert(3);
-//window.onload();
-//alert(3);
-
+function mapExcel(arr,func,val){
+    for(var i=0;i < row;i++) {
+        for(var j = 0; j < col; j++){
+            func(arr[i][j],val);
+        }
+    }
+}
 function initGame(){
     emptyDivs = [];
     scores = 0;
-    document.getElementById("score").innerHTML = scores;
+    document.getElementById("score").innerHTML = scores.toString();
     var endPage = document.getElementById("end");
     endPage.style.display="none";
-    //// TODO: 出现了很多次的4 这个魔幻数字，修改
-    for(var i=0;i < row;i++) {
-        for (var j = 0; j < col; j++) {
-            setDivValue(gameExcel[i][j],invalidValue);
-            gameExcel[i][j].innerHTML = invalidValue;
-            gameExcel[i][j].setAttribute("class","myGrid");
-        }
-    }
+    mapExcel(gameExcel,setDivValue,invalidValue);
 
 
     refreshEmptyDivs(emptyDivs);
@@ -54,8 +47,6 @@ function initGame(){
 
 function initGridItems() {
     var game = document.getElementById("Game");
-
-    //// TODO: div必须要代码生成吗？
     for (var i = 0; i < row; i++) {
         var arr = [];
         for (var j = 0; j < col; j++) {
@@ -86,8 +77,6 @@ function setDivValue(divElem, value){
     else{
         divElem.setAttribute("class", makeClassName(value));
     }
-
-
 }
 
 function getDivValue(divElem){
@@ -96,15 +85,11 @@ function getDivValue(divElem){
 
 function refreshEmptyDivs(empty_divs){
     empty_divs.length = 0;
-
-    for(var i=0;i<row;i++){
-        for(var j=0;j<col;j++){
-            if(invalidValue === getDivValue(gameExcel[i][j])){
-                empty_divs.push(gameExcel[i][j]);
+    mapExcel(gameExcel,function(arr,val){
+            if(val === getDivValue(arr)){
+                empty_divs.push(arr);
             }
-        }
-    }
-
+    },invalidValue);
     return empty_divs;
 }
 
@@ -114,8 +99,7 @@ function fillOneDivItem(empty_divs){
     setDivValue(empty_divs[nNumber], num);
 }
 
-//// TODO: leftAddNum是什么意思？
-//// TODO: 这个函数必须这么长？缩短
+
 function leftMoveSum(divs) {
     var add = false;
     for (var i = 0; i < row; i++) {
@@ -126,7 +110,7 @@ function leftMoveSum(divs) {
 
                 add = true;
                 scores += parseInt(getDivValue(gameExcel[i][j]));
-                document.getElementById("score").innerHTML = scores;
+                document.getElementById("score").innerHTML = scores.toString();
 
                 if((j+1)==1 && gameExcel[i][j+2].innerHTML != invalidValue && gameExcel[i][j+2].innerHTML == gameExcel[i][j+3].innerHTML){
                     setDivValue(gameExcel[i][j+2], 2 * gameExcel[i][j+2].innerHTML);
@@ -134,43 +118,40 @@ function leftMoveSum(divs) {
 
                     add = true;
                     scores += parseInt(getDivValue(gameExcel[i][j+2]));
-                    document.getElementById("score").innerHTML = scores;
+                    document.getElementById("score").innerHTML = scores.toString();
                 }
             }
         }
     }
     return add;
-
 }
 
-//// TODO: 修改为递归形式
-//// TODO: left_move 和 up_move很像，能不能合并?
 function leftMove(divs){
     var move = false;
     for(var i=0;i<row;i++){
-        var m = -1;
-        for (var j = 0; j < divs[i].length; ++j){
-            //// TODO: 魔幻比较方式，建议修改
-            if (invalidValue === getDivValue(divs[i][j])){
-                if (-1 === m){
-                    m = j;
-                }
-            }
-            else{
-                if (-1 != m){
-                    //// TODO: 这个地方建议修改，逻辑太乱
-                    setDivValue(divs[i][m], getDivValue(divs[i][j]));
-                    setDivValue(divs[i][j], invalidValue);
-                    m++;
-                    move = true;
-                }
+        for(var j=0;j<col-1;j++){
+            if(getDivValue(divs[i][j])=== invalidValue){
+                move = true;
+                leftMoveInRow(divs,i,j,j+1);
+                break;
             }
         }
     }
     return move;
 }
 
-//// TODO: leftRightReverse函数命名不合适
+function leftMoveInRow(divs, i,j,index){
+    for (var column = index; column < divs[i].length; ++column){
+        if (getDivValue(divs[i][column])!= invalidValue){
+            setDivValue(divs[i][j], divs[i][column].innerHTML);
+            setDivValue(divs[i][column], invalidValue);
+
+            leftMoveInRow(divs ,i,j+1,column+1);
+            return ;
+        }
+    }
+}
+
 function rowReverse(divs){
     for (var i=0;i<row;i++){
         divs[i].reverse();
@@ -178,9 +159,6 @@ function rowReverse(divs){
     return divs;
 }
 
-
-//// TODO: upAddNum是什么意思？
-//// TODO: 这个函数必须这么长？缩短
 function upMoveSum(divs) {
     var add = false;
     for (var j = 0; j < col; j++) {
@@ -191,7 +169,7 @@ function upMoveSum(divs) {
 
                 add = true;
                 scores += parseInt(getDivValue(gameExcel[i][j]));
-                document.getElementById("score").innerHTML = scores;
+                document.getElementById("score").innerHTML = scores.toString();
 
                 if ((i + 1) == 1 && gameExcel[i + 2][j].innerHTML != invalidValue && gameExcel[i + 2][j].innerHTML == gameExcel[i + 3][j].innerHTML) {
                     setDivValue(gameExcel[i + 2][j], 2 * gameExcel[i + 2][j].innerHTML);
@@ -199,14 +177,13 @@ function upMoveSum(divs) {
 
                     add = true;
                     scores += parseInt(getDivValue(gameExcel[i+2][j]));
-                    document.getElementById("score").innerHTML = scores;
+                    document.getElementById("score").innerHTML = scores.toString();
                 }
             }
         }
     }
     return add;
 }
-
 
 function upMove(divs){
     var move = false;
@@ -231,7 +208,7 @@ function upMove(divs){
     return move;
 }
 
-function cloReverse(divs){
+function colReverse(divs){
     for (var j=0;j<col;j++){
         for(var i=0;i<row/2;i++){
             var arr = divs[i][j];
@@ -242,87 +219,118 @@ function cloReverse(divs){
     return divs;
 }
 
-//// TODO: 函数太长
+    function gameJudgment(condition1,condition2){
+        if(condition1 || condition2){
+            refreshEmptyDivs(emptyDivs);
+            fillOneDivItem(emptyDivs);
+        }
+        else {
+            refreshEmptyDivs(emptyDivs);
+            if (emptyDivs.length < 1) {
+                gameOver();
+            }
+        }
+    }
+
+    function process_left_key_down(){
+        var moveLeft = leftMove(gameExcel);
+        var addLeft = leftMoveSum(gameExcel);
+        leftMove(gameExcel);
+        gameJudgment(moveLeft,addLeft);
+    }
+
+    function process_up_key_down(){
+        var moveUp = upMove(gameExcel);
+        var addUp = upMoveSum(gameExcel);
+        upMove(gameExcel);
+        gameJudgment(moveUp,addUp);
+    }
+
+    function process_right_key_down(){
+        rowReverse(gameExcel);
+        var moveRight =leftMove(gameExcel);
+        var addRight =leftMoveSum(gameExcel);
+        leftMove(gameExcel);
+        rowReverse(gameExcel);
+        gameJudgment(moveRight,addRight);
+    }
+
+    function process_down_key_down(){
+        colReverse(gameExcel);
+        var moveDown = upMove(gameExcel);
+        var addDown = upMoveSum(gameExcel);
+        upMove(gameExcel);
+        colReverse(gameExcel);
+        gameJudgment(moveDown,addDown);
+    }
+
 document.onkeydown=function(e){
     e=window.event||e;
 
-    //// TODO：相比switch case，有没有更好的方式?
-    switch(e.keyCode){
-        case 37: //左键
-            var moveLeft = leftMove(gameExcel);
-            var addLeft = leftMoveSum(gameExcel);
-            leftMove(gameExcel);
+    var func_mapper = [
+        {
+            key_code: 37,
+            func: process_left_key_down
+        },
+        {
+            key_code: 38,
+            func: process_up_key_down
+        },
+        {
+            key_code: 39,
+            func: process_right_key_down
+        },
+        {
+            key_code: 40,
+            func: process_down_key_down
+        }
+    ];
 
-            if(moveLeft || addLeft){
-                refreshEmptyDivs(emptyDivs);
-                fillOneDivItem(emptyDivs);
-            }
-            else {
-                refreshEmptyDivs(emptyDivs);
-                if (emptyDivs.length < 1) {
-                    gameOver();
-                }
-            }
-
-
+    for ( var i= 0;i<func_mapper.length;i++){
+        if (func_mapper[i].key_code === e.keyCode){
+            func_mapper[i].func();
             break;
-        case 38: //向上键
-            var moveUp = upMove(gameExcel);
-            var addUp = upMoveSum(gameExcel);
-            upMove(gameExcel);
-
-            if(moveUp || addUp) {
-                refreshEmptyDivs(emptyDivs);
-                fillOneDivItem(emptyDivs);
-            }
-            else {
-                refreshEmptyDivs(emptyDivs);
-                if (emptyDivs.length < 1) {
-                    gameOver();
-                }
-            }
-            break;
-
-        case 39: //右键
-            rowReverse(gameExcel);
-            var moveRight =leftMove(gameExcel);
-            var addRight =leftMoveSum(gameExcel);
-            leftMove(gameExcel);
-            rowReverse(gameExcel);
-
-            if(moveRight || addRight) {
-                refreshEmptyDivs(emptyDivs);
-                fillOneDivItem(emptyDivs);
-            }
-            else {
-                refreshEmptyDivs(emptyDivs);
-                if (emptyDivs.length < 1) {
-                    gameOver();
-                }
-            }
-            break;
-
-        case 40: //向下键
-            cloReverse(gameExcel);
-            var moveDown = upMove(gameExcel);
-            var addDown = upMoveSum(gameExcel);
-            upMove(gameExcel);
-            cloReverse(gameExcel);
-
-            if(moveDown || addDown) {
-                refreshEmptyDivs(emptyDivs);
-                fillOneDivItem(emptyDivs);
-            }
-            else {
-                refreshEmptyDivs(emptyDivs);
-                if (emptyDivs.length < 1) {
-                    gameOver();
-                }
-            }
-            break;
-        default:
-            break;
+        }
     }
+
+
+
+    //switch(e.keyCode){
+    //    case 37: //左键
+    //        var moveLeft = leftMove(gameExcel);
+    //        var addLeft = leftMoveSum(gameExcel);
+    //        leftMove(gameExcel);
+    //        gameJudgment(moveLeft,addLeft);
+    //        break;
+    //
+    //    case 38: //向上键
+    //        var moveUp = upMove(gameExcel);
+    //        var addUp = upMoveSum(gameExcel);
+    //        upMove(gameExcel);
+    //        gameJudgment(moveUp,addUp);
+    //        break;
+    //
+    //    case 39: //右键
+    //        rowReverse(gameExcel);
+    //        var moveRight =leftMove(gameExcel);
+    //        var addRight =leftMoveSum(gameExcel);
+    //        leftMove(gameExcel);
+    //        rowReverse(gameExcel);
+    //        gameJudgment(moveRight,addRight);
+    //        break;
+    //
+    //    case 40: //向下键
+    //        cloReverse(gameExcel);
+    //        var moveDown = upMove(gameExcel);
+    //        var addDown = upMoveSum(gameExcel);
+    //        upMove(gameExcel);
+    //        cloReverse(gameExcel);
+    //        gameJudgment(moveDown,addDown);
+    //        break;
+    //
+    //    default:
+    //        break;
+    //}
 };
 
 
@@ -332,111 +340,10 @@ function gameOver() {
     var gameAgain = document.getElementById("again");
 
     gameAgain.onclick = function(){
-        //// FIX: 这前面的代码为什么不在initGame中？
-
         initGame();
     };
-
 }
 
-//})();
-
-
-(function() {
-    var endPage = document.getElementById("end");
-    endPage.style.display="inline";
-    var gameAgain = document.getElementById("again");
-
-    gameAgain.onclick = function(){
-        //// FIX: 这前面的代码为什么不在initGame中？
-
-        initGame();
-    };
-
 })();
-//function Person(n){
-//    var age = 0;
-//    var address = "a";
-//    var name = n;
-//
-//    return {
-//        getName : function (){
-//            return name;
-//        },
-//        changeInfo: function (a, addr){
-//            age = a;
-//            address = addr;
-//        },
-//        getAge: function (){
-//            return age;
-//        },
-//        getAddress: function (){
-//            return address;
-//        },
-//        getAge1: function (){
-//            return age;
-//        }
-//    };
-//}
-//
-//var p = Person("lsy");
-//alert(p.getName());
-//
-//p.changeInfo(1, "b");
-//alert(p.getAddress());
-//alert(p.getAge());
-//
-
-
-//var f = function (){};
-//function f(){};
-
-
-//var f_test = function (){
-//    alert(2);
-//};  // 匿名函数对象
-//f_test();
-
-
-
-//function f111(){
-//    var iii = 0;
-//    alert(iii);
-//
-//    return function (){
-//        alert(iii);
-//    };
-//}
-//
-//var bbb = f111();
-//bbb();
-
-
-
-//function sum_array(arr, sum_func){
-//    var ret_arr = [];
-//    for (var i = 0; i < arr.length; i = i+2){
-//        ret_arr.push(sum_func(arr[i], arr[i+1]));
-//    }
-//
-//    return ret_arr;
-//}
-//
-//var arr_t = [0,1,2,3,4,5,6,7];
-
-
-//alert(sum_array(arr_t, function (i, j){    return i + j;}));
-
-//alert(sum_array(arr_t, function (i, j){
-//    return i*j;
-//}));
-
-
-//alert(sum_array(arr_t, function (i){
-//    return function (i1,j1){
-//        i+=1;
-//        return i1 + j1 + i;
-//    }
-//}(9)));
 
 
